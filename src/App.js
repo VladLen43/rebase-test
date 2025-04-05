9cfad7f65f969abf7201a505648ee94c9db286b3
Vladislav Bogdanov <v.bogdanov@spectr.dev>
2025-04-05 18:57:43 +0300

    Fifth commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Fourth commit
+          Fifth commit
         </a>
       </header>
     </div>
